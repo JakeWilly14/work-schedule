77cfd4779c61c01b744c0b8f36e5050cd08cfde2
Jakewilly14 <jakewilly14@gmail.com>
2023-10-21 05:33:17 -0500

    Added JS timeblock variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,41 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// assigning variable to the current date
+var currentDay = dayjs().format('dddd MMMM D,');
+// inserting current date into 'p' element with currentDay ID
+$('#currentDay').text(currentDay);
+// assigning variables to time block in a call to jQuery
+var nineAm = $('#hour-9');
+nineAm = dayjs().hour(9).hour();
+
+var tenAm = $('#hour-10');
+nineAm = dayjs().hour(10).hour();
+
+var elevenAm = $('#hour-11');
+nineAm = dayjs().hour(11).hour();
+
+var twelvePm = $('#hour-12');
+nineAm = dayjs().hour(12).hour();
+
+var onePm = $('#hour-13');
+nineAm = dayjs().hour(13).hour();
+
+var twoPm = $('#hour-14');
+nineAm = dayjs().hour(14).hour();
+
+var threePm = $('#hour-15');
+nineAm = dayjs().hour(15).hour();
+
+var fourPm = $('#hour-16');
+nineAm = dayjs().hour(16).hour();
+
+var fivePm = $('#hour-17');
+nineAm = dayjs().hour(17).hour();
+// assigning variable to current hour of day
+var currentHour = dayjs().hour();
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
